e2d196040290511c2ff6fa7a89ab3464e94cadf9
anuj sharma <as9737856@gmail.com>
2025-06-29 16:03:01 +0530

    Update code

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
-import ServicesPage from './pages/ServicesPage'
-
+import ServicesPage from './pages/ServicesPage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* default route handling  */ }
+        <Route path="*" element={<LandingPage />} />
+
         <Route path="/" element={<LandingPage />} />
-        <Route path='/services' element={<ServicesPage/>}></Route>
-        
+
+        {/* Other routes */}
+        <Route path="/services" element={<ServicesPage />} />
       </Routes>
     </Router>
   );
